e00df7555cf00522c77b30c96123e016541d9ef8
MalakWaleed00 <mickywaleed2004@gmail.com>
2026-07-02 23:35:43 +0300

    for deployment(6) for frontend

--- src/main/java/com/example/demo/common/config/SecurityConfig.java
+++ src/main/java/com/example/demo/common/config/SecurityConfig.java
@@ -63,7 +63,9 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://localhost:3000",
-                "http://localhost:8080"
+                "http://localhost:8080",
+                "https://*.vercel.app"
+
         ));
         config.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS"
